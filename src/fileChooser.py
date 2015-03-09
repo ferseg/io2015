5d506baeb11cf2844d0ffb97680b183d22be1582
kennethms64 <kennethms64@gmail.com>
2015-03-08 22:58:57 -0600

    Se movieron las clases de interfaz al paquete userInterface

--- src/fileChooser.py
+++
@@ -1,7 +0,0 @@
-import tkinter
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = tkinter.filedialog.askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
