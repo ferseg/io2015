cc58d7e1ae56c16092ae81de34253f7d112f99b3
Fernando Segovia <fernandosegovia25@gmail.com>
2015-06-13 19:26:44 -0600

    Simplex with unbounded solution improved

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ __author__ = 'fsegovia'
 
 # Testing
 
-from userInterface.plotter import plot_graph
-from userInterface.plotter import matrix_to_inequation
+#from userInterface.plotter import plot_graph
+#from userInterface.plotter import matrix_to_inequation
 
 
 
@@ -13,19 +13,20 @@ import math_utils as leu
 import simplex as si
 
 
-simp = si.Simplex([1], 2)
+simp = si.Simplex([[-2, -1, 0, 0, 0], [1, -1, 1, 0, 10], [2, 0, 0, 1, 42]], 2)
 
 #Ejemplo 1
 #mat = [[-1,-1,-2,0,0,0,0],[2,1,1,1,0,0,50], [2,1,0,0,-1,0,36], [1,0,1,0,0,-1,10]]
 #Ejemplo 2 (VISTO EN CLASES)
-mat = [[-3, -2, 0, 0, 0, 0], [2, 1, 1, 0, 0, 18], [2, 3, 0, 1, 0, 42], [3, 1, 0, 0, 1, 24]]
-simp.set_matrix(mat)
+#mat = [[-3, -2, 0, 0, 0, 0], [2, 1, 1, 0, 0, 18], [2, 3, 0, 1, 0, 42], [3, 1, 0, 0, 1, 24]]
+#simp.set_matrix(mat)
 print(simp.start_simplex())
 print(simp.get_matrix())
+print(simp.solution_type)
 
 
 
-matrix_to_inequation([[1,0,"<=",8],[0,1,"<=",4]])
+#matrix_to_inequation([[1,0,"<=",8],[0,1,"<=",4]])
 
 
 #plot_graph(["8","x*0+4"],[[0,4],[8,4],[0,0],[8,0]],[8,4])
